471fc06e9b31c32f1ce13114890115f9e4e708dd
Noël Herwig <n.herwig16@gmail.com>
2025-12-18 15:49:41 +0100

    Replaced sidebar toggle icon

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,21 +1,11 @@
+<script setup lang="ts">
+import { PanelRightOpenIcon } from 'lucide-vue-next'
+</script>
+
 <template>
   <nav class="navbar w-full bg-base-300 fixed z-10">
     <label for="sidebar-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
-      <!-- Sidebar toggle icon -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        stroke-linejoin="round"
-        stroke-linecap="round"
-        stroke-width="2"
-        fill="none"
-        stroke="currentColor"
-        class="my-1.5 inline-block size-4"
-      >
-        <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
-        <path d="M9 4v16" />
-        <path d="M14 10l2 2l-2 2" />
-      </svg>
+      <PanelRightOpenIcon :size="16" class="rotate-180" />
     </label>
 
     <div class="px-4">
