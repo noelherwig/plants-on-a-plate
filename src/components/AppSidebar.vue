d86f1afb5c23b51f1578ef1294f2d329cf993f6c
NMG <n.herwig16@gmail.com>
2025-12-22 17:17:56 +0100

    Added active link styling to sidebar

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -10,18 +10,18 @@ import SidebarLink from './SidebarLink.vue'
     <aside
       class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64"
     >
-      <nav aria-label="Sidebar navigation">
+      <nav class="w-full" aria-label="Sidebar navigation">
         <ul class="menu w-full grow">
-          <SidebarLink to="/" label="Home">
-            <LeafIcon :size="20" class="text-green-500" aria-hidden="true" />
+          <SidebarLink to="/" label="Home" color="green">
+            <LeafIcon :size="20" aria-hidden="true" />
           </SidebarLink>
 
-          <SidebarLink to="/favorites" label="Favorites">
-            <HeartIcon :size="20" class="text-pink-500" aria-hidden="true" />
+          <SidebarLink to="/favorites" label="Favorites" color="pink">
+            <HeartIcon :size="20" aria-hidden="true" />
           </SidebarLink>
 
-          <SidebarLink to="/shopping-list" label="Shopping List">
-            <ShoppingBasketIcon :size="20" class="text-blue-500" aria-hidden="true" />
+          <SidebarLink to="/shopping-list" label="Shopping List" color="blue">
+            <ShoppingBasketIcon :size="20" aria-hidden="true" />
           </SidebarLink>
         </ul>
       </nav>
--- src/components/SidebarLink.vue
+++ src/components/SidebarLink.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-defineProps<{ to: string; label: string }>()
+const props = defineProps<{ to: string; label: string; color: 'green' | 'pink' | 'blue' }>()
+
+// Static class maps for Tailwind compatibility
+const bgClasses: Record<string, string> = {
+  blue: 'dark:bg-blue-700 bg-blue-500',
+  green: 'dark:bg-green-700 bg-green-500',
+  pink: 'dark:bg-pink-700 bg-pink-500',
+}
 </script>
 
 <template>
@@ -9,9 +16,11 @@ defineProps<{ to: string; label: string }>()
       :aria-label="label"
       class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3"
       :data-tip="label"
+      :class="`text-${color}-500`"
+      :activeClass="`${bgClasses[props.color]} text-white`"
     >
       <slot />
-      <span class="is-drawer-close:hidden h-5 text-nowrap">{{ label }}</span>
+      <span class="is-drawer-close:hidden h-5 text-white">{{ label }}</span>
     </RouterLink>
   </li>
 </template>
