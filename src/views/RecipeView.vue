37a09f485be7bcfda761d1669a3fd5c8d06d3e20
Noël Herwig <n.herwig16@gmail.com>
2025-12-28 13:33:05 +0100

    Added SaveButton to RecipeView

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -6,6 +6,7 @@ import BackButton from '@/components/BackButton.vue'
 import RecipeDetails from '@/components/RecipeDetails.vue'
 import CookingSteps from '@/components/CookingSteps.vue'
 import { useRecipeStore } from '@/stores/recipeStore'
+import SaveButton from '@/components/SaveButton.vue'
 
 const props = defineProps<{ id: string }>()
 
@@ -17,7 +18,11 @@ const recipe = computed(() => recipeStore.getById(props.id))
   <div class="container max-w-300 mx-auto">
     <div class="flex justify-between mb-4">
       <BackButton />
-      <FavoriteButton v-if="recipe" :recipe-id="recipe.id" :recipe-title="recipe.title" />
+
+      <div v-if="recipe" class="flex gap-2">
+        <SaveButton :recipe-id="recipe.id" :recipe-title="recipe.title" />
+        <FavoriteButton :recipe-id="recipe.id" :recipe-title="recipe.title" />
+      </div>
     </div>
 
     <template v-if="recipe">
