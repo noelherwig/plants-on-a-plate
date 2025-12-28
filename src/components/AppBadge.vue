73c5b4265da5f08ea83a4c503a2ab09cb7cafa39
Noël Herwig <n.herwig16@gmail.com>
2025-12-28 12:10:09 +0100

    Updated AppBadge spacing

--- src/components/AppBadge.vue
+++ src/components/AppBadge.vue
@@ -5,7 +5,9 @@ defineProps<{ icon?: FunctionalComponent; label: string }>()
 </script>
 
 <template>
-  <div class="badge bg-(--color-blue-content) text-(--color-blue) border-0">
+  <div
+    class="badge bg-(--color-blue-content) text-(--color-blue) border-0 px-2 flex items-center gap-1"
+  >
     <component :is="icon" :size="16" aria-hidden="true" />
     <span>{{ label }}</span>
   </div>
