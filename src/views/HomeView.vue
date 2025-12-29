d69621cf48189114b509e834b4472a1aaf14dfa6
NMG <n.herwig16@gmail.com>
2025-12-29 16:03:16 +0100

    Added skeleton components to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,28 +19,40 @@ const { searchResults } = useSearch(searchTerm, recipes)
   <div class="container max-w-300 mx-auto">
     <h1 class="text-xl font-bold mb-4 flex items-center gap-2">
       <LeafIcon class="text-(--color-green)" aria-hidden="true" />
-      All plant-based recipes
+      <span>All plant-based recipes</span>
     </h1>
 
-    <div class="flex flex-col sm:flex-row items-end">
+    <div class="flex flex-col sm:flex-row items-end gap-4">
       <RecipeSearch v-model="searchTerm" />
-      <div class="flex items-center">
-        <p class="mx-4" aria-hidden="true">or</p>
+      <div class="flex items-center gap-4">
+        <span aria-hidden="true">or</span>
         <RandomRecipeButton :recipes="recipes" />
       </div>
     </div>
 
     <hr class="my-8 h-0.5 border-t-0 bg-neutral" role="separator" />
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" v-if="recipes.length > 0">
       <RecipeCard v-for="recipe in searchResults" :key="recipe.id" :recipe="recipe" />
+    </div>
+
+    <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6" aria>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
+      <div class="skeleton h-65"></div>
     </div>
 
     <p
       v-if="!searchResults.length && searchTerm"
       class="col-span-full text-center text-base-content/60"
     >
-      No recipes found with "{{ searchTerm }}". Try a different search term!
+      No recipes found with "{{ searchTerm }}". Try a different search term.
     </p>
   </div>
 </template>
