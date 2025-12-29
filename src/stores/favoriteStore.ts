201f5e1c32e5c8b0319ed86cf91cb38ff59a4c84
NMG <n.herwig16@gmail.com>
2025-12-29 12:42:44 +0100

    Added localStorage to favoriteStore and savedStore

--- src/stores/favoriteStore.ts
+++ src/stores/favoriteStore.ts
@@ -1,23 +1,31 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useRecipeStore } from './recipeStore'
+
+const FAVORITES_KEY = 'favoriteRecipeIds'
 
 export const useFavoriteStore = defineStore('favorite', () => {
   const recipeStore = useRecipeStore()
 
-  const favoriteIds = ref<string[]>([])
+  const localStorageIds = localStorage.getItem(FAVORITES_KEY)
+  const favoriteIds = ref<string[]>(localStorageIds ? JSON.parse(localStorageIds) : [])
   const favorites = computed(() =>
     recipeStore.recipes.filter(({ id }) => favoriteIds.value.includes(id)),
   )
 
   const isFavorite = (id: string) => favoriteIds.value.includes(id)
 
-  const add = (id: string) => favoriteIds.value.push(id)
+  const add = (id: string) => !isFavorite(id) && favoriteIds.value.push(id)
 
   const remove = (id: string) =>
     (favoriteIds.value = favoriteIds.value.filter((favoriteId) => favoriteId !== id))
 
   const toggle = (id: string) => (isFavorite(id) ? remove(id) : add(id))
+
+  watch(favoriteIds, (ids) => localStorage.setItem(FAVORITES_KEY, JSON.stringify(ids)), {
+    // Tell Vue to watch nested properties (IDs) instead of the array itself
+    deep: true,
+  })
 
   return { favorites, isFavorite, remove, add, toggle }
 })
--- src/stores/savedStore.ts
+++ src/stores/savedStore.ts
@@ -1,21 +1,29 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useRecipeStore } from './recipeStore'
+
+const SAVED_KEY = 'savedRecipeIds'
 
 export const useSavedStore = defineStore('saved', () => {
   const recipeStore = useRecipeStore()
 
-  const savedIds = ref<string[]>([])
+  const localStorageIds = localStorage.getItem(SAVED_KEY)
+  const savedIds = ref<string[]>(localStorageIds ? JSON.parse(localStorageIds) : [])
   const saved = computed(() => recipeStore.recipes.filter(({ id }) => savedIds.value.includes(id)))
 
   const isSaved = (id: string) => savedIds.value.includes(id)
 
-  const save = (id: string) => savedIds.value.push(id)
+  const save = (id: string) => !isSaved(id) && savedIds.value.push(id)
 
   const unsave = (id: string) =>
     (savedIds.value = savedIds.value.filter((savedId) => savedId !== id))
 
   const toggle = (id: string) => (isSaved(id) ? save(id) : unsave(id))
+
+  watch(savedIds, (ids) => localStorage.setItem(SAVED_KEY, JSON.stringify(ids)), {
+    // Tell Vue to watch nested properties (IDs) instead of the array itself
+    deep: true,
+  })
 
   return { saved, isSaved, save, unsave, toggle }
 })
