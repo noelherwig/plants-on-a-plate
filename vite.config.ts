0f27934b4dbf545d6cbf8dd6b763889dd31f1226
NMG <n.herwig16@gmail.com>
2025-12-30 14:54:04 +0100

    Added PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueDevTools()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Plants on a Plate',
+        short_name: 'Plants on a Plate',
+        description: 'Plants on a Plate',
+        theme_color: '#0d0d0d',
+        background_color: '#0d0d0d',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            purpose: 'maskable',
+            sizes: '192x192',
+            src: 'maskable_icon_x192.png',
+            type: 'image/png',
+          },
+          {
+            purpose: 'maskable',
+            sizes: '512x512',
+            src: 'maskable_icon_x512.png',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
