3fa3dce9f57affa9b4cf72519ced5e48cd397c88
NMG <n.herwig16@gmail.com>
2026-01-09 12:40:12 +0100

    Fixed bug in BackButton

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -3,11 +3,13 @@ import { ChevronLeftIcon } from 'lucide-vue-next'
 import { useRouter } from 'vue-router'
 
 const $router = useRouter()
+
+const goBack = () => (history.state.back ? $router.back() : $router.push({ name: 'Home' }))
 </script>
 
 <template>
   <div class="lg:tooltip lg:tooltip-right" data-tip="Go back">
-    <button @click="$router.back()" class="btn btn-square btn-ghost" aria-label="Go back">
+    <button @click="goBack()" class="btn btn-square btn-ghost" aria-label="Go back">
       <ChevronLeftIcon aria-hidden="true" />
     </button>
   </div>
