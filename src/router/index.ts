3741bcf6c8a5dc2152909b86f210ecee2bdcf21e
NMG <n.herwig16@gmail.com>
2026-01-09 12:35:15 +0100

    Added title metadata to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,28 +13,48 @@ const router = createRouter({
       path: '/favorites',
       name: 'Favorites',
       component: () => import('../views/FavoritesView.vue'),
+      meta: {
+        title: 'Favorites',
+      },
     },
     {
       path: '/saved',
       name: 'Saved',
       component: () => import('../views/SavedView.vue'),
+      meta: {
+        title: 'Saved',
+      },
     },
     {
       path: '/recipe/:id',
       name: 'Recipe',
       component: () => import('../views/RecipeView.vue'),
+      meta: {
+        title: 'Recipe',
+      },
       props: true,
     },
     {
       path: '/shopping-list',
       name: 'Shopping list',
       component: () => import('../views/ShoppingListView.vue'),
+      meta: {
+        title: 'Shopping list',
+      },
     },
     {
       path: '/:pathMatch(.*)*',
       component: () => import('../views/PageNotFoundView.vue'),
+      meta: {
+        title: '404',
+      },
     },
   ],
+})
+
+router.beforeEach(async (to) => {
+  const pageTitle = to.meta.title
+  document.title = pageTitle ? `Plants on a Plate - ${pageTitle}` : 'Plants on a Plate'
 })
 
 export default router
